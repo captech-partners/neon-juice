5b8fe6d60317e4463cd441343b5eecf9eadf1f1d
Chaitrika Budamagunta <cbudamagunta@dons.usfca.edu>
2020-03-23 15:45:19 -0700

    Start building React App

---
+++ assembler/studio/app/Components/App.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+
+/**
+ * Renders a react-router enabled app with a wrapper to facilitate shared styles
+ * and markup; add new routes for pages here.
+ */
+const App = () => {
+  return (
+    <div className="home-page">
+      <h1>Website Creation Studio</h1>
+    </div>
+  );
+};
+
+modules.exports = App;
---
+++ assembler/studio/app/index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './Components/App';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
