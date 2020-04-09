a281f7b28ce5093ea245596dfd9fd9ecee52834e
Vanessa Villalobos <vvillalobos123@gmail.com>
2020-04-08 22:31:29 -0700

    Adding TextField

--- assembler/studio/app/components/NewTemplate/index.js
+++ assembler/studio/app/components/NewTemplate/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+/*
 const NewTemplate = () => {
 
   return (
@@ -7,6 +8,57 @@ const NewTemplate = () => {
       <h1>NewTemplate</h1>
     </div>
   );
+};
+*/
+
+
+//import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const NewTemplate = () => {
+    const Button = styled.button`
+        background: #E5C1EE;
+        border-radius: 3px;
+        border: solid #DBB7E4;
+        color: #33153A;
+        font-size: .5em;
+        margin: 0 1em;
+        padding: 0.25em 1em;
+    `
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            '& .MuiTextField-root': {
+                margin: theme.spacing(1),
+                width: '25ch',
+            },
+        },
+    }));
+
+    export default function MultilineTextFields() {
+        const classes = useStyles();
+        const [value, setValue] = React.useState('Controlled');
+
+        const handleChange = (event) => {
+            setValue(event.target.value);
+        };
+
+        return (
+            <form className={classes.root} noValidate autoComplete="off">
+                <div>
+                    <TextField
+                        id="standard-textarea"
+                        label="Name"
+                        placeholder="Placeholder"
+                        multiline
+                    />
+                    <Button>Done</Button>
+                </div>
+            </form>
+        );
+    }
+
 };
 
 export default NewTemplate;
