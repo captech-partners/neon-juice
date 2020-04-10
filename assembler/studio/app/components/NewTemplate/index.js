da955bddc5b95f30b91884f647515c63934198c3
Vanessa Villalobos <vvillalobos123@gmail.com>
2020-04-09 18:01:15 -0700

    Update index.js

--- assembler/studio/app/components/NewTemplate/index.js
+++ assembler/studio/app/components/NewTemplate/index.js
@@ -2,11 +2,55 @@ import React from 'react';
 
 const NewTemplate = () => {
 
-  return (
-    <div className="new-template">
-      <h1>NewTemplate</h1>
-    </div>
-  );
+    state = {
+        dataLabel: "",
+        dataPage: "",
+        template: "",
+        content: "",
+    };
+
+    change = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        console.log(this.state);
+    };
+
+    render() {
+        return (
+            <form>
+                <input 
+                    name="dataLabel"
+                    placeholder="Data Label" 
+                    value={this.state.dataLabel} 
+                    onChange={e => this.change(e)}
+                />
+                <input 
+                    name="dataPage"
+                    placeholder="Data Page" 
+                    value={this.state.dataPage} 
+                    onChange={e => this.change(e)}
+                />
+                <input 
+                    name="template"
+                    placeholder="Template" 
+                    value={this.state.template} 
+                    onChange={e => this.change(e)}
+                />
+                <input 
+                    name="content"
+                    placeholder="Content" 
+                    value={this.state.content} 
+                    onChange={e => this.change(e)}
+                />
+                <button onClick={e => this.onSubmit(e)}>Submit</button>
+            </form>
+        );
+    }
 };
 
 export default NewTemplate;
