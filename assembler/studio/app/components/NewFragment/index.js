2e91c81ae3ee92c73eaff022721517bc5123e697
Chaitrika Budamagunta <cbudamagunta@dons.usfca.edu>
2020-04-19 14:25:40 -0700

    Rebuild

--- assembler/studio/app/components/NewFragment/index.js
+++ assembler/studio/app/components/NewFragment/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import HTMLEditor from '../HTMLEditor';
+
 const NewFragment = () => {
 
   return (
     <div className="new-fragment">
       <h1>NewFragment</h1>
+
+      <HTMLEditor />
     </div>
   );
 };
