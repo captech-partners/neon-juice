be1c4e7dc3dfccf0e05d848d7debf7254978a868
Chaitrika Budamagunta <cbudamagunta@dons.usfca.edu>
2020-04-28 22:17:52 -0700

    Set up page preview

--- assembler/studio/app/components/EditFragment/index.js
+++ assembler/studio/app/components/EditFragment/index.js
@@ -14,10 +14,6 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 
-import Iframe from 'react-iframe'
-
-
-
 import styled from 'styled-components';
 
 const InputFields = styled.div`
--- assembler/studio/app/pages/Studio/index.js
+++ assembler/studio/app/pages/Studio/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
-
-import { BrowserRouter as Router, Redirect, Route, Link, Switch } from 'react-router-dom';
 import styled from 'styled-components';
 
 import FragmentTemplateList from '../../components/FragmentTemplateList';
-
-import EditFragment from '../../components/EditFragment';
-import EditTemplate from '../../components/EditTemplate';
-import NewFragment from '../../components/NewFragment';
-import NewTemplate from '../../components/NewTemplate';
-
-const Button = styled.button`
-  background: #E5C1EE;
-  border-radius: 3px;
-  border: solid #DBB7E4;
-  color: #33153A;
-  font-size: .5em;
-  margin: 0 1em;
-  padding: 0.25em 1em;
-`
 
 const Container = styled.div`
   margin: .5em;
