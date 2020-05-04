ab972993f21e710c5bc5f3d6381dfa1e34ddd569
Chelsy Simran <chels.simran@gmail.com>
2020-05-04 11:46:45 -0700

    added default styling

--- assembler/studio/app/components/GlobalDetails/index.js
+++ assembler/studio/app/components/GlobalDetails/index.js
@@ -90,13 +90,237 @@ class GlobalDetails extends React.Component {
         </label>
         <input type="submit" value="Submit" />
       </form>
-          <Button variant="primary" onClick={e => this.onExport(e)}>Export</Button>
-          <Button variant="primary" onClick={e => this.goToStudio(e)}>Go To Studio</Button>
+          <br/>
+          <Button variant="info" onClick={e => this.goToStudio(e)}>Go To Studio</Button>
+          <br/>
+          <br/>
+          <Button variant="success" onClick={e => this.onExport(e)}>Export</Button>
         </InputFields>
 
         <Main>
-                <CodeMirror value={"<div> </div>"}
-                onChange={this.handleChange} options={options}/>
+                <h2>Styling</h2>
+                <CodeMirror value="
+            html {
+              width: 100%;
+              height: 100%;
+              padding: 0px;
+              margin: 0px;
+          }
+
+          body {
+              width: 100%;
+              height: 100%;
+              padding: 0px;
+              margin: 0px;
+              margin: 0px;
+              background-color: #f5f4f2;
+          }
+
+          h1 {
+              font-size: 1.5em;
+              font-weight: bold;
+              line-height: 0.5em;
+              color: #0e3368;
+          }
+
+          h2 {
+              font-size: 2.5em;
+              line-height: 0.5em;
+              color: #0e3368;
+          }
+
+          h3 {
+              line-height: 0.1em;
+              margin: 0px;
+              margin-top: 7px;
+              padding: 0px;
+              color: #0e3368;
+          }
+
+          img {
+              max-width: 100%;
+              max-height: 100%;
+          }
+
+          .header {
+              width: 100%;
+              border-bottom: solid 1px #ccc;
+              background-color: #fff;
+          }
+
+          .cart {
+              display: inline-block;
+              float: right;
+              font-size: 1.8em;
+              padding: 10px;
+          }
+
+          .cart:hover {
+              color: #db3832;
+              cursor: pointer;
+          }
+
+          .nav {
+              width: 100%;
+              display: inline-block;
+          }
+
+          .nav-left {
+              float: left;
+              display: inline-block;
+              padding: 0 10px 10px 10px;
+          }
+
+          .nav-right {
+              float: right;
+              display: inline-block;
+              padding: 0 10px 10px 10px;
+          }
+
+          .nav-element {
+              display: inline-block;
+              font-size: 1.2em;
+              cursor: pointer;
+              margin-right: 10px;
+          }
+
+          .nav-element:hover {
+              color: #db3832;
+              cursor: pointer;
+          }
+
+          .content {
+              width: 100%;
+              text-align: center;
+          }
+
+          .panel-top {
+              width: 90%;
+              max-width: 1190px;
+              background-color: #fff;
+              border-radius: 10px;
+              display: inline-block;
+              margin-top: 20px;
+          }
+
+          .panel-content {
+              display: inline-block;
+              width: 19.6%;
+              vertical-align: top;
+              margin: 0px;
+              padding-top: 25px;
+              padding-bottom: 25px;
+              border-right: #f5f4f2 1px solid;
+          }
+
+          .panel-content:hover {
+              color: #db3832;
+              cursor: pointer;
+          }
+
+          .panel-icon {
+              font-size: 2em;
+          }
+
+          .panel-main {
+              width: 90%;
+              display: inline-block;
+              margin-top: 20px;
+          }
+
+          .panel-main-left {
+              width: 590px;
+              display: inline-block;
+              margin: 0px;
+              margin-right: 6px;
+              padding: 0px;
+              vertical-align: top;
+              margin-bottom: 10px;
+          }
+
+          .panel-main-right {
+              width: 590px;
+              display: inline-block;
+              margin: 0px;
+              padding: 0px;
+              vertical-align: top;
+          }
+
+          .panel-main-right-box {
+              width: 290px;
+              border: solid 1px #ccc;
+              height: 189px;
+              background-color: #fff;
+              border-radius: 5px;
+              display: inline-block;
+              text-align: left;
+              vertical-align: top;
+              margin-bottom: 5px;
+          }
+
+          .panel-main-right-box:hover {
+              border: solid 1px #db3832;
+              cursor: pointer;
+          }
+
+          .panel-main-left-content {
+              text-align: left;
+              background-color: #fff;
+              border: solid 1px #ccc;
+              border-radius: 5px;
+              overflow: hidden;
+          }
+
+          .panel-main-left-content:hover {
+              border: solid 1px #db3832;
+              cursor: pointer;
+          }
+
+          .content-details {
+              padding: 10px;
+          }
+
+          .panel-special {
+              width: 90%;
+              max-width: 1190px;
+              display: inline-block;
+          }
+
+          .panel-special-content {
+              display: inline-block;
+              width: 100%;
+              text-align: left;
+          }
+
+          .special-wrapper {
+              width: 100%;
+              max-width: 1190px;
+          }
+
+          .special-item {
+              width: 184px;
+              height: 140px;
+              border: solid 1px #095ba5;
+              border-radius: 5px;
+              margin-right: 11px;
+              margin-bottom: 10px;
+              display: inline-block;
+              text-align: center;
+              vertical-align: top;
+              background-color: #fff;
+              overflow: hidden;
+          }
+
+          .special-item:hover {
+              border: solid 1px #db3832;
+              cursor: pointer;
+          }
+
+          .special-image {
+              width: 100px;
+          }"
+        
+        onChange={this.handleChange} options={options}/>
         </Main>
       </div>
     )
