9aa904f937554d59f32b6207fb88511ed30aa3f1
Priscilla Hamiter <phamiter253@gmail.com>
2020-10-21 20:39:43 -0700

    adding folder

---
+++ assembler/rapidweb/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  Driver  from './components/Driver';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Driver />
+    </div>
+  );
+}
+
+export default App;
---
+++ assembler/rapidweb/src/components/CodeEditor.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/monokai.css";
+import "codemirror/addon/display/autorefresh";
+import { UnControlled as CodeMirror } from "react-codemirror2";
+require("codemirror/mode/xml/xml");
+require("codemirror/mode/javascript/javascript");
+require("codemirror/mode/htmlmixed/htmlmixed");
+
+class CodeEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      outputText: props.outputText,
+    };
+  }
+
+  handleChange(value) {
+    this.props.onHtmlChange(value);
+  }
+
+  render() {
+    return (
+      <div>
+        <CodeMirror
+          value={this.state.outputText}
+          options={{
+            mode: "htmlmixed",
+            theme: "monokai",
+            lineNumbers: true,
+            lineWrapping: true,
+            autoRefresh: true,
+          }}
+          onChange={(editor, data, value) => {
+            this.handleChange(value);
+          }}
+        />
+      </div>
+    );
+  }
+}
+export default CodeEditor;
